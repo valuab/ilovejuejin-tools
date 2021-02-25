f02bb2580c5a9471972770a0a874aef4e4fd7489
valuab <1363907445@qq.com>
2021-02-25 09:45:33 +0800

    fix:  调整部分数据模块

--- api/apiPublic/modules/comment.ts
+++ api/apiPublic/modules/comment.ts
@@ -1,3 +1,125 @@
 /**
  * 评论相关接口
  */
+import { NuxtAxiosInstance } from '@nuxtjs/axios'
+import { IApiResult } from '../index'
+import { handleUrlParams } from '~/utils/data'
+
+const commentLink = {
+  getNewsCommentList: '/api/comment/getNewsCommentList', // 搜索全部
+  getNewCommentReplyList: '/api/comment/getNewCommentReplyList', // 分类搜索
+  supportComment: '/api/comment/supportComment', // 标签搜索
+  postComment: '/api/comment/postComment', // 主持人搜索
+}
+
+/**
+ * 参数约束 START
+ */
+interface INewsCommentListParmas {
+  id: number
+  typeId: number
+  sort: number
+}
+interface INewCommentReplyListParmas {
+  id: number
+  contentId: number
+  sort: number
+}
+interface ISupportCommentParmas {
+  commentId: number
+  contentId: number
+  shardId: number
+  shardTypeId: number
+}
+interface IPostCommentParmas {
+  commentId: number
+  contentId: number
+  shardId: number
+  content: string
+  djcarsmid: number
+}
+
+/**
+ * 参数约束 END
+ */
+
+/**
+ * 输出解耦 START
+ */
+
+interface ApiCommentDto {
+  id: String
+  userId: number
+  typeId: number
+  contentId: string
+  shardId: string
+}
+
+export interface NewsCommentListResult extends IApiResult {
+  result: ApiCommentDto
+}
+export interface NewCommentReplyListResult extends IApiResult {
+  result: ApiCommentDto
+}
+export interface SupportCommentResult extends IApiResult {
+  result: {
+    commentId: number
+    shardId: number
+    contentId: number
+    shardTypeId: number
+  }
+}
+export interface PostCommentResult extends IApiResult {
+  result: {
+    commentId: number
+    shardId: number
+    contentId: number
+    content: string
+    djcarsmid: number
+  }
+}
+/**
+ * 输出解耦 END
+ */
+
+export interface ICommentModule {
+  getNewsCommentList(
+    params: INewsCommentListParmas
+  ): Promise<NewsCommentListResult['result']>
+  getNewCommentReplyList(
+    params: INewCommentReplyListParmas
+  ): Promise<NewCommentReplyListResult['result']>
+  supportComment(
+    params: ISupportCommentParmas
+  ): Promise<SupportCommentResult['result']>
+  postComment(params: IPostCommentParmas): Promise<PostCommentResult['result']>
+}
+
+export default ($axios: NuxtAxiosInstance) => {
+  return {
+    async getNewsCommentList(params) {
+      const url = handleUrlParams(commentLink.getNewsCommentList, params)
+      const { data } = await $axios.get<NewsCommentListResult>(url)
+
+      return data.result
+    },
+    async getNewCommentReplyList(params) {
+      const url = handleUrlParams(commentLink.getNewCommentReplyList, params)
+      const { data } = await $axios.get<NewCommentReplyListResult>(url)
+
+      return data.result
+    },
+    async supportComment(params) {
+      const url = handleUrlParams(commentLink.supportComment, params)
+      const { data } = await $axios.get<SupportCommentResult>(url)
+
+      return data.result
+    },
+    async postComment(params) {
+      const url = handleUrlParams(commentLink.postComment, params)
+      const { data } = await $axios.get<PostCommentResult>(url)
+
+      return data.result
+    },
+  } as ICommentModule
+}
--- api/apiPublic/modules/search.ts
+++ api/apiPublic/modules/search.ts
@@ -4,17 +4,147 @@
 
 import { NuxtAxiosInstance } from '@nuxtjs/axios'
 import { IApiResult } from '../index'
+import { handleUrlParams } from '~/utils/data'
 
 const searchLink = {
+  searchAll: '/api/search/searchAll', // 搜索全部
+  searchByItemCategoryId: '/api/search/searchByItemCategoryId', // 分类搜索
+  searchByItemKeywordId: '/api/search/searchByItemKeywordId', // 标签搜索
+  searchByHostUserId: '/api/search/searchByHostUserId', // 主持人搜索
+  searchByCars: '/api/search/searchByCars', // 搜索车型
   getKolList: '/api/search/getHostList', // 获取王牌节目列表
 }
 
-export interface IKolListType {
-  userId: number
-  nickname: string
-  smallImageUrl: string
-  postCount: string
+/**
+ * 参数约束 START
+ */
+interface ISearchAllParmas {
+  keyword: string
+  viewUserId: number
 }
+interface ISearchByItemCategoryIdParmas {
+  keyword: string
+  categoryId: number
+  viewUserId: number
+}
+interface ISearchByItemKeywordIdParmas {
+  keyword: string
+  keywordId: number
+  viewUserId: number
+}
+interface ISearchByHostUserIdParmas {
+  keyword: string
+  hostUserId: number
+  viewUserId: number
+}
+interface ISearchByCarsParmas {
+  keyword: string
+  categoryId: number
+  keywordId: number
+  hostUserId: number
+  viewUserId: number
+}
+
+/**
+ * 参数约束 END
+ */
+
+/**
+ * 输出解耦 START
+ */
+export interface ISearchAll extends IApiResult {
+  result: {
+    forumId: number
+    postId: number
+    title: string
+    smallImageUrl: string
+    totalViewCount: number
+    commentCount: number
+    publishTime: String
+    userId: number
+    userName: String
+    itemKeywordName: String
+    itemCategoryName: String
+  }
+}
+export interface ISearchByItemCategoryId extends IApiResult {
+  result: {
+    forumId: number
+    postId: number
+    title: string
+    smallImageUrl: string
+    totalViewCount: number
+    commentCount: number
+    publishTime: String
+    userId: number
+    userName: String
+    itemKeywordName: String
+    itemCategoryName: String
+  }
+}
+export interface ISearchByItemKeywordId extends IApiResult {
+  result: {
+    forumId: number
+    postId: number
+    title: string
+    smallImageUrl: string
+    totalViewCount: number
+    commentCount: number
+    publishTime: String
+    userId: number
+    userName: String
+    itemKeywordName: String
+    itemCategoryName: String
+  }
+}
+export interface ISearchByHostUserId extends IApiResult {
+  result: {
+    forumId: number
+    postId: number
+    title: string
+    smallImageUrl: string
+    totalViewCount: number
+    commentCount: number
+    publishTime: String
+    userId: number
+    userName: String
+    itemKeywordName: String
+    itemCategoryName: String
+  }
+}
+
+export interface ISearchByCars extends IApiResult {
+  result: {
+    forumId: number
+    postId: number
+    title: string
+    smallImageUrl: string
+    totalViewCount: number
+    commentCount: number
+    publishTime: String
+    userId: number
+    userName: String
+    itemKeywordName: String
+    itemCategoryName: String
+  }
+}
+
+export interface IKolListType extends IApiResult {
+  result: {
+    forumId: number
+    postId: number
+    title: string
+    smallImageUrl: string
+    totalViewCount: number
+    commentCount: number
+    publishTime: String
+    userId: number
+    userName: String
+    itemKeywordName: String
+    itemCategoryName: String
+  }
+}
+
 export interface IKolListResult extends IApiResult {
   result: {
     total: number
@@ -22,16 +152,65 @@ export interface IKolListResult extends IApiResult {
   }
 }
 
+/**
+ * 输出解耦 END
+ */
+
 export interface ISearchModule {
-  getKolList(): Promise<IKolListType[]>
+  getSearchAll(params: ISearchAllParmas): Promise<ISearchAll['result']>
+  getSearchByItemCategoryId(
+    params: ISearchByItemCategoryIdParmas
+  ): Promise<ISearchByItemCategoryId['result']>
+  getSearchByItemKeywordId(
+    params: ISearchByItemKeywordIdParmas
+  ): Promise<ISearchByItemKeywordId['result']>
+  getSearchByHostUserId(
+    params: ISearchByHostUserIdParmas
+  ): Promise<ISearchByHostUserId['result']>
+  getSearchByCars(params: ISearchByCarsParmas): Promise<ISearchByCars['result']>
+  getKolList(): Promise<IKolListResult['result']>
 }
 
 export default ($axios: NuxtAxiosInstance) => {
-  const searchModule: ISearchModule = {
+  return {
+    async getSearchAll(params) {
+      const url = handleUrlParams(searchLink.searchAll, params)
+      const { data } = await $axios.get<ISearchAll>(url)
+
+      return data.result
+    },
+
+    async getSearchByItemCategoryId(params) {
+      const url = handleUrlParams(searchLink.searchByItemCategoryId, params)
+      const { data } = await $axios.get<ISearchByItemCategoryId>(url)
+
+      return data.result
+    },
+
+    async getSearchByItemKeywordId(params) {
+      const url = handleUrlParams(searchLink.searchByItemKeywordId, params)
+      const { data } = await $axios.get<ISearchByItemKeywordId>(url)
+
+      return data.result
+    },
+
+    async getSearchByHostUserId(params) {
+      const url = handleUrlParams(searchLink.searchByHostUserId, params)
+      const { data } = await $axios.get<ISearchByHostUserId>(url)
+
+      return data.result
+    },
+
+    async getSearchByCars(params) {
+      const url = handleUrlParams(searchLink.searchByCars, params)
+      const { data } = await $axios.get<ISearchByCars>(url)
+
+      return data.result
+    },
+
     async getKolList() {
       const { data } = await $axios.get<IKolListResult>(searchLink.getKolList)
-      return data.result.list
+      return data.result
     },
-  }
-  return searchModule
+  } as ISearchModule
 }
