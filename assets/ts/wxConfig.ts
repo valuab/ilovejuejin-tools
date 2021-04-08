f71a78092c8c3760f54dfae2f9126aabd711cc12
yangsw <yangsw@djcars.cn>
2021-04-08 18:08:39 +0800

    chore: 添加 appid 判断

--- assets/ts/wxConfig.ts
+++ assets/ts/wxConfig.ts
@@ -5,7 +5,7 @@ const isProduction = process.env.NODE_ENV === 'production'
 
 export default {
   WX_URL: 'https://api.weixin.qq.com',
-  APP_ID: 'wxa255ebd2f771f5e2',
+  APP_ID: isProduction ? 'wxa255ebd2f771f5e2' : 'wxc02745329e7866a5',
   // 扫码重定向URL
   REDIRECT_URL: isProduction
     ? 'https://pc-beta.djcars.cn'
