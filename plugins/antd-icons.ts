9e9b2ac99621530d17ed5d1fc6b3ddb0f333c43c
songlh <songlh@djcars.cn>
2021-04-21 10:14:14 +0800

    fix: 引入分页箭头图标

--- plugins/antd-icons.ts
+++ plugins/antd-icons.ts
@@ -2,6 +2,10 @@ export { default as LeftOutline } from '@ant-design/icons/lib/outline/LeftOutlin
 export { default as RightOutline } from '@ant-design/icons/lib/outline/RightOutline'
 export { default as SearchOutline } from '@ant-design/icons/lib/outline/SearchOutline'
 
+// 分页箭头图标
+export { default as DoubleRightOutline } from '@ant-design/icons/lib/outline/DoubleRightOutline'
+export { default as DoubleLeftOutline } from '@ant-design/icons/lib/outline/DoubleLeftOutline'
+
 // message 图标
 export { default as CloseCircleFill } from '@ant-design/icons/lib/fill/CloseCircleFill'
 export { default as CheckCircleFill } from '@ant-design/icons/lib/fill/CheckCircleFill'
