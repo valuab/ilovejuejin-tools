99d1f7e8ac76adbb3cced20c61f27b13a06b156d
songlh <songlh@djcars.cn>
2021-05-14 15:16:04 +0800

    fix: 修复分页自定义指令点击区域过大

--- plugins/directives.ts
+++ plugins/directives.ts
@@ -2,8 +2,11 @@ import Vue from 'vue'
 
 Vue.directive('anchor', {
   inserted: (el, binding) => {
-    el.onclick = function () {
-      if (binding.value) {
+    el.onclick = function (event) {
+      const target = event.target as HTMLElement
+      const nodeName = target?.nodeName.toLocaleLowerCase()
+
+      if (binding.value && nodeName === 'a') {
         const vnode = document.getElementById(binding.value) as HTMLElement
         document.documentElement.scrollTop = vnode?.offsetTop || 0
       }
