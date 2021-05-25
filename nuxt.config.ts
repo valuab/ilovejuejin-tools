283b2b0fc8ac088eaf0cf4ebd8924ac3d1c26cc0
yangsw <yangsw@djcars.cn>
2021-05-25 16:43:41 +0800

    refactor: 去掉手机端视口配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,7 @@ export default {
     },
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      // { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { 'http-equiv': 'X-UA-Compatible', content: 'ie=edge,chrome=1' },
       {
         name: 'keywords',
